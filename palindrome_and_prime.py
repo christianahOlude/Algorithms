86bb8dad308a39fc94a261dd5bfacc9430630fbe
christianahOlude <oludechristianah16@gmail.com>
2025-08-12 15:32:24 +0100

    Palindrome

--- palindrome_and_prime.py
+++ palindrome_and_prime.py
@@ -1,11 +1,10 @@
 def is_palindrome(number):
-    num = str(number)
-    reverse = num[::-1]
-    if reverse == num:
+    converted_number = str(number)
+    reverse = converted_number[::-1]
+    if reverse == converted_number:
         return True
     else:
         return False
-
 
 def is_prime(number, num):
     if number / num == 1:
