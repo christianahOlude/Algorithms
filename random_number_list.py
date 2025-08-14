bc110e6cdeb4fd65f10f851b73a0f5cdeee3295e
christianahOlude <oludechristianah16@gmail.com>
2025-08-14 07:57:27 +0100

    calculate average, multiply third position, sum odd position, sum even position, largest and smallest numbers

--- random_number_list.py
+++ random_number_list.py
@@ -11,3 +11,20 @@ def length_of_list(my_list):
         counter += 1
     return counter
 
+def sum_of_even_position(my_list):
+    return sum(my_list[1::2])
+
+def sum_of_odd_position(my_list):
+    return sum(my_list[::2])
+
+def multiply_of_third_position(my_list):
+    return my_list[2] * my_list[5] * my_list[8]
+
+def calculate_average(my_list):
+    return sum(my_list) / len(my_list)
+
+def largest_number(my_list):
+    return max(my_list)
+
+def smallest_number(my_list):
+    return min(my_list)
--- test_random_number_list.py
+++ test_random_number_list.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from random_number_list import random_number_list, length_of_list
+from random_number_list import random_number_list, length_of_list, sum_of_even_position, sum_of_odd_position, \
+    multiply_of_third_position, calculate_average, largest_number, smallest_number
 
 
 class Test(TestCase):
@@ -15,4 +16,32 @@ class Test(TestCase):
         self.assertEqual(5, actual)
         print(actual)
 
+    def test_sum_of_even_position(self):
+        number = [1,2,3,4,5]
+        actual = sum_of_even_position(number)
+        self.assertEqual(6, actual)
 
+    def test_sum_of_odd_position(self):
+        number = [1,2,3,4,5]
+        actual = sum_of_odd_position(number)
+        self.assertEqual(9, actual)
+
+    def test_multiply_of_third_position(self):
+        number = [1,2,3,4,5,6,7,8,9,10]
+        actual = multiply_of_third_position(number)
+        self.assertEqual(162, actual)
+
+    def test_calculate_average(self):
+        number = [1,2,3,4,5,6,7,8,9,10]
+        actual = calculate_average(number)
+        self.assertEqual(5.5, actual)
+
+    def test_largest_number(self):
+        number = [1,2,3,4,5]
+        actual = largest_number(number)
+        self.assertEqual(5, actual)
+
+    def test_smallest_number(self):
+        number = [0,1,2,3,4,5]
+        actual = smallest_number(number)
+        self.assertEqual(0, actual)
