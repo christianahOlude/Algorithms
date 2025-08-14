70b278d87cff897d0e2105121757954d0cde6316
christianahOlude <oludechristianah16@gmail.com>
2025-08-14 13:43:15 +0100

    calculate average, multiply third position, sum odd position, sum even position without inbuilt functions

--- random_number_list.py
+++ random_number_list.py
@@ -1,9 +1,7 @@
 import random
 
-
 def random_number_list(number):
     return [random.randint(1, 50) for numbers in range(number)]
-
 
 def length_of_list(my_list):
     counter = 0
@@ -12,16 +10,32 @@ def length_of_list(my_list):
     return counter
 
 def sum_of_even_position(my_list):
-    return sum(my_list[1::2])
+    even_position = my_list[1::2]
+
+    summ = 0
+    for num in even_position:
+         summ += num
+    return summ
+
 
 def sum_of_odd_position(my_list):
-    return sum(my_list[::2])
+    odd_position = my_list[::2]
+
+    summ = 0
+    for num in odd_position:
+        summ += num
+    return summ
 
 def multiply_of_third_position(my_list):
     return my_list[2] * my_list[5] * my_list[8]
 
 def calculate_average(my_list):
-    return sum(my_list) / len(my_list)
+    summ = 0
+    counter = 0
+    for num in my_list:
+        counter += 1
+        summ += num
+    return summ / counter
 
 def largest_number(my_list):
     return max(my_list)
