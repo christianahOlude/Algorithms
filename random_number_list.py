a16ea4cb67592a623c520819ad484fd8e3a7e4b6
christianahOlude <oludechristianah16@gmail.com>
2025-08-14 06:44:32 +0100

    Length of a list without using len() function

--- random_number_list.py
+++ random_number_list.py
@@ -1,12 +1,13 @@
 import random
-from itertools import count
 
 
 def random_number_list(number):
     return [random.randint(1, 50) for numbers in range(number)]
 
 
-def length_of_list(number):
-   for numbers in range(number):
-       return count(numbers)
-   return None
+def length_of_list(my_list):
+    counter = 0
+    for num in my_list:
+        counter += 1
+    return counter
+
--- test_random_number_list.py
+++ test_random_number_list.py
@@ -1,7 +1,4 @@
 from unittest import TestCase
-
-from samba.dcerpc.security import ace_object
-
 from random_number_list import random_number_list, length_of_list
 
 
@@ -16,3 +13,6 @@ class Test(TestCase):
         number = [1,2,3,4,5]
         actual = length_of_list(number)
         self.assertEqual(5, actual)
+        print(actual)
+
+
